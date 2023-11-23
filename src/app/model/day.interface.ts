0c43421af52532be6196a8a6c6149b388684a1b4
LateLuca03 <luca.laterza03@gmail.com>
2023-11-23 15:44:01 +0100

    css dettaglio continuato

--- src/app/model/day.interface.ts
+++ src/app/model/day.interface.ts
@@ -1,15 +1,16 @@
 export interface Day {
-    timepoint: number,
-    cloudcover: number,
-    seeing: number,
-    trasparency: number,
-    lifetd_index: number,
-    rh2m: number,
+    timepoint: number;
+    cloudcover: number;
+    seeing: number;
+    trasparency: number;
+    lifetd_index: number;
+    rh2m: number;
     wind10m: {
-        direction: "string",
-        speed: number,
-    }
-    temp2m: number,
-    prec_type: string,
-    image: string
+        direction: "string";
+        speed: number;
+    };
+    temp2m: number;
+    prec_type: string;
+    image: string;
+    timepointToHour: string;
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,6 +8,7 @@ import { Sun } from "../model/sun.interface";
     providedIn: "root",
 })
 export class ApiService {
+
     baseUrlSun = `https://api.sunrisesunset.io/json?`;
 
     baseUrlDay = `https://www.7timer.info/bin/astro.php?ac=0&unit=metric&output=json&tzshift=0&`;
@@ -25,20 +26,35 @@ export class ApiService {
     getDayData(lat: string, long: string) {
         return this.http.get(this.baseUrlDay + `lon=${long}&lat=${lat}`).pipe(
             map((response: any) => {
+                const currentDate: Date = new Date();
                 response.dataseries.forEach((element: any) => {
+                    if (element.cloudcover <= 2) {
+                        element.image = "../assets/img/sun.png";
+                    } else if (
+                        element.cloudcover > 2 &&
+                        element.cloudcover <= 7
+                    ) {
+                        element.image = "../assets/img/little-cloudy.png";
+                    } else if (response.cloudcover > 7) {
+                        element.image = "../assets/img/cloudy.png";
+                    }
 
-                if (element.cloudcover <= 2) {
-                    element.image = "../assets/img/sun.png";
-                } else if (
-                    element.cloudcover > 2 &&
-                    element.cloudcover <= 7
-                ) {
-                    element.image = "../assets/img/little-cloudy.png";
-                } else if (response.cloudcover > 7) {
-                    element.image = "../assets/img/cloudy.png";
-                }
-                
-            });
+                    const timepointDate = new Date(
+                        currentDate.getTime() +
+                            element.timepoint * 60 * 60 * 1000
+                    );
+
+                    let hours: string = String(timepointDate.getHours());
+                    let minutes: string = String(timepointDate.getMinutes());
+
+                    if (timepointDate.getHours() < 10)
+                        hours = "0" + timepointDate.getHours();
+
+                    if (timepointDate.getMinutes() < 10)
+                        minutes = "0" + timepointDate.getMinutes();
+
+                    element.timepointToHour = hours + ":" + minutes;
+                });
 
                 return response.dataseries as Day[];
             })
